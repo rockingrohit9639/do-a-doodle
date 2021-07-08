8520c5010e920cd445fb2f0a3ffa0e2467f1d8ca
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2021-07-08 10:44:27 +0530

    frontend whiteboard draw ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from "react";
 import './App.css';
 
-function App() {
+function App()
+{
+
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+
+  const startDrawing = ({ nativeEvent }) =>
+  {
+    const { offsetX, offsetY } = nativeEvent;
+    console.log(true);
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(offsetX, offsetY);
+    setIsDrawing(true);
+  }
+
+  const endDrawing = () =>
+  {
+    console.log(false);
+    contextRef.current.closePath();
+    setIsDrawing(false);
+  }
+
+  const draw = ({ nativeEvent }) =>
+  {
+    const { offsetX, offsetY } = nativeEvent;
+    if(!isDrawing) return;
+
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+  }
+
+
+  useEffect(() =>
+  {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const context = canvas.getContext("2d");
+    context.scale(2, 2);
+    context.lineCap = "round";
+    context.strokeStyle = "black";
+    context.lineWidth = 5;
+    contextRef.current = context;
+
+    window.addEventListener("resize", () =>
+    {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    })
+
+  }, []);
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <canvas
+        ref={canvasRef}
+        id="canvas"
+        onMouseDown={startDrawing}
+        onMouseUp={endDrawing}
+        onMouseMove={draw}
+      >
+      </canvas>
+
     </div>
   );
 }
